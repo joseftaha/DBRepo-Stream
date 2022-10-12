014770c45d1220fb646e221be70dafa9bc56d997
Josef Taha <58595842+joseftaha@users.noreply.github.com>
2022-10-13 01:56:06 +0200

    Update and rename umweltbundesamt.py to airquality.py

--- stream_data/src/airquality.py
+++ stream_data/src/airquality.py
@@ -9,31 +9,68 @@ import pika
 import json
 import numpy as np
 import dbrepo as dbr
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 
+
+
+# load enviorment variables
+url = os.getenv('DBREPO_HTTP_URL')
+broker_url = os.getenv('DBREPO_RABBITMQ_BROKER_URL')
+broker_port = os.getenv('DBREPO_RABBITMQ_BROKER_PORT')
+print(url, broker_url, broker_port)
+
+user = os.getenv('AIRQUALITY_DBREPO_USERNAME')
+passw = os.getenv('AIRQULAITY_DBREPO_PASSWORD')
+print(user, passw)
+
+db_name = os.getenv('AIRQULAITY_DBREPO_DB_NAME')
+db_desc = os.getenv('AIRQUALITY_DBREPO_DB_DESCRIPTION')
+print(db_name, db_desc)
+dbid = cid = None
+
+# client connection to dbrepo
+client = dbr.Client(username=user,password=passw,url=url, verifyTLS=False)
+
+
+# table columns name for airquality data
 col = ['stationid','component','time','unit','meantype','value','meta_name','meta_owner',
             'meta_location','x_coord','y_coord','z_coord']
 
+# table columns type for airquality data
 col_type = ['string','string','number','string','string','decimal','string','string','string','decimal','decimal','decimal']
 
-
-
+# table indexes for airquality data
 index_col = ['stationid','component','time']
 
+
+
+
+################################################################
+## helper functions to persist airquality data locally 
+################################################################
 def get_current_file_path() -> str:
     tz = pytz.timezone('Europe/Berlin')
     now = datetime.now(tz)
     year, week, weekday = now.isocalendar()
     return f'data/poll/data_{year}_{week}_{weekday}.csv'
 
-def get_current_dbrepo_table_name() -> str:
-    tz = pytz.timezone('Europe/Berlin')
-    now = datetime.now(tz)
-    year, week, weekday = now.isocalendar()
-    table_name = f'data_{year}_{week}'
-    table_desc = f'This table provides real-time air-data from around 170 air measuring stations in Austria from week {week} of year {year}.'
-    return table_name, table_desc
+def open_current_file() -> pd.DataFrame:
+    try:
+        return pd.read_csv(get_current_file_path())
+    except:
+        return pd.DataFrame()
 
+def persist_current_dataframe(df: pd.DataFrame) -> None:
+        df.to_csv(get_current_file_path())
+
+
+################################################################
+## helper functions to manage dbrepo database and table 
+## for airquality data 
+################################################################
 def generate_dbrepo_table(name, desc):
     columns = [{
             "name": x,
@@ -51,15 +88,10 @@ def get_current_tables_in_database(cid, dbid) -> list:
     data = client.fetch_database_info(cid, dbid)
     return data['name']
 
-def open_current_file() -> pd.DataFrame:
-    try:
-        return pd.read_csv(get_current_file_path())
-    except:
-        return pd.DataFrame()
 
-def persist_current_dataframe(df: pd.DataFrame) -> None:
-        df.to_csv(get_current_file_path())
-
+################################################################
+## method for scraping airquality data from unweltbundesamt.at
+################################################################
 def extract_airpollution_data() -> pd.DataFrame:
 
     df = pd.DataFrame()
@@ -80,8 +112,6 @@ def extract_airpollution_data() -> pd.DataFrame:
         data = pd.DataFrame(data)
         df = pd.concat([df,data])
 
-    # df = df.pivot_table(values='value', index=['stationid','time'], columns='compname', aggfunc='first')
-    # df = df.set_index(index_col)
     df = df.rename(columns={
     'compname':'component',
     'gml$Point_gml$coord_X':'x_coord',
@@ -100,45 +130,40 @@ def extract_airpollution_data() -> pd.DataFrame:
 
     return df
 
+
+################################################################
+## method for sending data tuples to dbrepo rabbit mq broker
+################################################################
 def send(df : pd.DataFrame):
 
-    table_name, desc = get_current_dbrepo_table_name()
+    # check if table for airquality already exists
     db_info = client.fetch_table_info(cid,dbid)
-    db_contains_table = not db_info.empty and db_info['name'].str.contains(table_name).any()
+    db_contains_table = not db_info.empty and db_info['name'].str.contains(db_name).any()
 
+    # generate database if not exist
     if not db_contains_table:
-        generate_dbrepo_table(table_name, desc)
+        generate_dbrepo_table(db_name, db_desc)
 
+    # connect to dbrepo broker via AMPQ
     credentials = pika.PlainCredentials(user, passw)
-    parameters = pika.ConnectionParameters(broker_url, 5672, '/', credentials)
+    parameters = pika.ConnectionParameters(broker_url, broker_port, '/', credentials)
     connection = pika.BlockingConnection(parameters=parameters)
     channel = connection.channel()
 
+    # send data tuples to dbrepo table
     for _, row in df.iterrows():
         payload = row.to_json()
-        channel.basic_publish(exchange=db_name.lower(), routing_key=table_name, body=payload)
+        channel.basic_publish(exchange=db_name.lower(), routing_key=db_name, body=payload)
 
+    # close AMPQ connection
     channel.close()
 
 
-
-
-
-
-url = 'https://dbrepo.ossdip.at'
-broker_url = '128.130.202.19'
-
-user = 'jtaha'
-passw = 'pw'
-
-db_name = 'AirQuality'
-db_desc = 'This database contains real-time airquality data from 170 stations in Austria || Data-Source: luft.umweltbundesamt.at'
-dbid = cid = None
-
-client = dbr.Client(username=user,password=passw,url=url, verifyTLS=False)
-
-
-
+################################################################
+## main function periodically scraping airquality data from
+## umweltbundesamt (every 30min) and sending new data to 
+## dbrepo rabbit mq broker
+################################################################
 def main():
 
     global dbid, cid, db_name
@@ -173,5 +198,7 @@ def main():
             send(new)
 
         sleep(600)
+
+
 
 main()
